da98af52b4cbdbbf27e2bc583f20f95522f8a278
Wamikmk <wamikmk@gmail.com>
2024-01-10 05:08:37 +0000

    container config done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+let valueInNumber = Number(score)
+
+//console.log(typeof(score))
+
+//console.log(valueInNumber)
+//console.log(typeof(valueInNumber))
+
+let num = "44abc"
+
+let newNum = Number(num)
+
+//console.log(newNum)
+//console.log(typeof(newNum))
+
+let someNum = 10
+console.log(typeof(someNum))
+
+let anotherNum = String(someNum)
+console.log(typeof(anotherNum))
+
+//"44" => 44
+// "44abc" => Nan
+
+// 1=> True 0=> False
+// "" => False "abs" => True
