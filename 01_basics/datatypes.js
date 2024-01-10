1e29a76a82ad71443bf3394a96457090439f9b00
Wamikmk <wamikmk@gmail.com>
2024-01-10 04:18:16 +0000

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+console.log("Wamik")
+
+
+
+// alert(3+3) // we are using nodejs, not browser
+// datatypes
+// number => 2
+// string => ""
+// boolean => True/False
+// null => standalone value
+// undefined => value not assigned
+// symbol => uniqueness
+
+// object
+
+console.log(typeof )
