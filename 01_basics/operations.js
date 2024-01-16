342af47110aefd4e05bfc269646952fa6d6275f6
Wamikmk <wamikmk@gmail.com>
2024-01-16 04:38:02 +0000

    container config done

---
+++ 01_basics/operations.js
@@ -0,0 +1,17 @@
+// Operations
+
+let a = 2
+let b = 4
+let nega = -a
+//console.log(nega) // prints negative value of a.
+//console.log(a+b) // addition
+//console.log(typeof(a)) // check datatype
+
+//console.log(a-b) // subtraction
+//console.log(a*b) // multiplication
+//console.log(a**b) // exponential
+//console.log(a/b) // division
+//console.log(a%b) // modulo // chk remainder
+
+console.log("2" + 3 + 1) // output => 231
+console.log(1 + 2 + "4") // output => 34
