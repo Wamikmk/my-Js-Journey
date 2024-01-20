e2e8df66a5408b356cb829c8c3fddba1d817c0e3
Wamiq Mushtaq <wamikmk@gmail.com>
2024-01-20 04:36:02 +0000

    container confg done

---
+++ 01_basics/strings.js
@@ -0,0 +1,35 @@
+//console.log("Hello " + "World")
+const name = "Wamik"
+const age = 20
+// console.log(name + age + " hello");
+
+//console.log(`Hello my name is ${name} and my age is ${age} `);
+// Back Ticks
+
+const gameName = new String("Wamikmk")
+//console.log(gameName[0]);
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase()) // methods
+// console.log(gameName.charAt(2)) // char at index 2.
+// console.log(gameName.indexOf('m')) // m at which index.
+
+// const newString = gameName.substring(0,5)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-6,2)
+// console.log(anotherString);
+
+// const newName = "   Wamik  " // trims widespaces
+// console.log(newName.trim());
+
+// console.log(newName.replace('k','q'));
+
+const myWebsite = "Wamik-mk-dot-com"
+// splitting the above string at '-' and turning it into an array.
+
+console.log(myWebsite.split('-'));
+
+
+
+
+// game name is an object.
