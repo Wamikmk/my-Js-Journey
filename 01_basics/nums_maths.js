07d860e23d6e2a2f26d43a688a0276dae76e3aeb
Wamiq Mushtaq <wamikmk@gmail.com>
2024-01-22 04:47:21 +0000

    container config done

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,38 @@
+// const num = 400
+// console.log(num);
+
+// const score = new Number(500)
+// console.log(score)
+
+
+// const newScore = score.toString()
+// console.log(newScore)
+// console.log(newScore.length)
+
+// const balance = new Number(300)
+// console.log(balance.toFixed(2));
+
+// const number = 1000000
+// console.log(number.toLocaleString('en-IN'))
+
+// ------------ MATHS --------------
+
+//let num = -4
+//console.log(Math.abs(num));
+
+// let anotherNum = 4.4
+// console.log(Math.round(anotherNum))
+
+// let nums = 4.2
+// console.log(Math.floor(nums))
+
+// let x = 9
+// let y = Math.sqrt(x)
+// console.log(`The Square root of ${x} is ${y}`)
+
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.floor(Math.random() * 10));
+console.log(Math.floor(Math.random() * 10)+1)
+
+// + 1 to avoid Zero
