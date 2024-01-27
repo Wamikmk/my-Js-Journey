766c910e1cdf0091d2dfae95541ea7196b528863
Wamiq Mushtaq <wamikmk@gmail.com>
2024-01-27 09:46:27 +0000

    container config done

---
+++ 02_basics/arrays_02.js
@@ -0,0 +1,36 @@
+let football_players = ['Messi', 'Rooney', 'Pogba']
+
+let tennis_players = ['Nadal', 'Novak', 'Murray']
+
+//football_players.push(tennis_players)
+
+//console.log(football_players);
+//console.log(football_players[3][1])
+
+// Two more functions
+// Concat
+
+// let allPlayers = football_players.concat(tennis_players)
+
+// console.log(allPlayers)
+
+// Note : concatenating two arrays needs to be held in a new array.
+
+// Spread : Similar to concatenate
+
+// let all_Players = [...football_players,...tennis_players]
+// console.log(all_Players)
+
+// let myName = 'Wamik'
+// console.log(Array.isArray(myName));
+// console.log(Array.from(myName))
+
+// let myObject = {name: 'Wamik'}
+
+// console.log(Array.from(myObject))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
