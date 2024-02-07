c9639ce0d0537b0440381f7bda53b7cf1bccd071
Wamiq Mushtaq <wamikmk@gmail.com>
2024-02-07 04:08:19 +0000

    container config done

---
+++ 03_basics/IIFE.js
@@ -0,0 +1,17 @@
+// ------------ Immediately Invoked Function Expression-----------
+
+// Wrap the function inside ()
+// To invoke use ()
+// While writing two IIFIs make sure to put ; after the first IIFE
+
+
+((names) => (console.log(`Hello ${names}`))) ("John") ;
+
+
+(function hello(names) {
+    console.log(`Hi ${names}`)
+}) ("Jenny")
+
+
+
+
