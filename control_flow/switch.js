89c41aedd7225a24ab505b0ace1bf96cfb1590cc
Wamiq Mushtaq <wamikmk@gmail.com>
2024-02-08 05:16:08 +0000

    container config done

---
+++ control_flow/switch.js
@@ -0,0 +1,25 @@
+const month = 3
+
+// Switch is an alternative for checking conditions which otherwise would require many if else statements.
+
+switch (month) {
+    case 1:
+        console.log("Jan")
+        
+        break;
+    case 2:
+        console.log("Feb")
+        
+        break;
+    case 3:
+        console.log("march")
+        
+        break;
+    case 4:
+        console.log("april")
+        
+        break;
+
+    default:
+        break;
+}
