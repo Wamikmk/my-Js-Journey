98041c6c2f2a6fe665aca7cc562350dc2b29eca6
Wamiq Mushtaq <wamikmk@gmail.com>
2024-02-11 05:15:59 +0000

    container config done

---
+++ loops/while.js
@@ -0,0 +1,25 @@
+let index = 0
+while (index <= 10) {
+    //console.log(index)
+    index = index + 1
+}
+
+let myArray = ['Jordan', 'Nadal', 'Khabib']
+
+let arr = 0
+
+while (arr < myArray.length) {
+    //console.log(myArray[arr])
+    arr = arr + 1 
+
+
+//---------------------- DO WHILE -------------------------
+
+//Checks condition later,executes code first.
+
+let score = 11
+
+do {
+    console.log(score)
+    score ++
+} while (score <=10)
