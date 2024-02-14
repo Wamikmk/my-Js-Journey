3f30021e7bd30fc5e241d0a2ea78a249cebc577f
Wamiq Mushtaq <wamikmk@gmail.com>
2024-02-14 04:03:32 +0000

    container config done

--- loops/reduce.js
+++ loops/reduce.js
@@ -1,14 +1,39 @@
-// Reduce Mathod
+// Reduce Method
+
+// Use case => Adding Shopping bill
 
 const myNums = [1,2,3,4,5]
 
-const initialValue = 0
+//const initialValue = 0
 
-const newTotal = myNums.reduce(
-    (accumulator, currentvalue) => accumulator + currentvalue, initialValue
+const shoppingCart = [ {
+    courseName : "JS",
+    price : 299
+},
 
+{
+    courseName : "PYTHON",
+    price : 599
+},
+
+{
+    courseName : "JAVA",
+    price : 999
+}
+
+]
+
+const courseTotal = shoppingCart.reduce(
+    (acc, item) => acc + item.price, 0
 )
+
+// const newTotal = myNums.reduce(
+//     (accumulator, currentvalue) => accumulator + currentvalue, initialValue
+
+// )
 
 // first time the accumulator has to be given initial value
 
-console.log(newTotal)
+//console.log(newTotal)
+
+console.log(courseTotal);
